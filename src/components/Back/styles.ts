15bdb5edc2628954fa73209f81144768e545a3bc
z3oxs <z3ox1s@protonmail.com>
2022-06-17 00:13:47 -0300

    fix: Fixed font call error

--- src/components/Back/styles.ts
+++ src/components/Back/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
     font-family: "Inter", sans-serif;
     color: #fff;
