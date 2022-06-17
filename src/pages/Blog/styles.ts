9299fad7c4ccb9cf594baefff4cd3413be44110f
z3oxs <z3ox1s@protonmail.com>
2022-06-16 23:26:54 -0300

    fix: Fixed blog mainpage

--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&display=swap');
 
     font-family: "Inter", sans-serif;
     display: flex;
